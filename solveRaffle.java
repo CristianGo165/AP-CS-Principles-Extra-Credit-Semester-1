faada9719c7574010a309ba9718b56bdb47feea3
CristianGo165 <137358764+CristianGo165@users.noreply.github.com>
2023-12-20 00:02:28 -0600

    Update solveRaffle.java
    Adds the binary search function to solveRaffle.java

--- solveRaffle.java
+++ solveRaffle.java
@@ -14,4 +14,25 @@ public class solveRaffle {
 
         return ":[" + " closest sum is " + sumStore;
     }
+    public static int BinarySearchTest(int[] data, int target){
+            int first = 0;
+            int last = data.length - 1;
+            int middle;
+            int count = 0;
+
+            while(first <= last){
+                middle = (first + last)/2;
+                if(target == data[middle]){
+                    count++;
+                    return count;
+                }else if(target < data[middle]){
+                    count++;
+                    last = middle - 1;
+                }else{
+                    count++;
+                    first = middle + 1;
+                }
+            }
+            return count;
+    }
 }
