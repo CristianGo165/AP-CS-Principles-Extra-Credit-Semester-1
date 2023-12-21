1184e0762e52cc03091a8817877445e9989cb6cc
CristianGo165 <137358764+CristianGo165@users.noreply.github.com>
2023-12-21 14:21:20 -0600

    Update raffle.java
    Updated W/. comments
    Slight code changes

--- raffle.java
+++ raffle.java
@@ -1,7 +1,7 @@
 import java.util.Random;
+import java.util.Arrays;
 
 public class raffle{
-
     public int[] generateRandomList(){
 
         Random random = new Random();
@@ -9,18 +9,22 @@ public class raffle{
         int extent = 1000;
         int[] randList = new int[listLength];
 
+        //Generates the list of random ticket numbers
         for (int i = 0; i < randList.length; i++) {
             randList[i] = random.nextInt(extent);
         }
 
+        //Sorts List
+        Arrays.sort(randList);
+
+        /*Prints list
         System.out.println("Raffle List");
-        //Prints list
         for (int i = 0; i < randList.length; i++) {
             System.out.println(randList[i]);
         }
-        //prints list
         System.out.println("Raffle List");
-
+        prints list*/
+        
         return randList;
     }
 
