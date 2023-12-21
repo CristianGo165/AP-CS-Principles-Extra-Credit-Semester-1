1251cfce84d9209954a730fffe8831c014b6c7f7
CristianGo165 <137358764+CristianGo165@users.noreply.github.com>
2023-12-21 14:27:15 -0600

    Update userInput.java
    Added Comments

--- userInput.java
+++ userInput.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 public class userInput {
     Scanner scanner = new Scanner(System.in);
     public int userPrompt(){
+        //Gets the winning number from the user
         System.out.println("Enter the winning raffle number! (0 - 1000): ");
         int winningNum = scanner.nextInt();
         System.out.println("Winning raffle is: " + winningNum);
